3ac4ffbb19a4c5278c3a70fc5b7fe21f22f0a401
koenigone <mm6dd.mh@gmail.com>
2025-02-09 14:12:50 +0000

    initialized project

---
+++ client/src/components/sidebar/sidebar.jsx
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react"
+import { createBreakpoints } from "@chakra-ui/theme-tools"
+
+const Sidebar = () => {
+    return
+    <>
+
+    </>
+}
+
+export default Sidebar
