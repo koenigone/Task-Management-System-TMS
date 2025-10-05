b20f80a13c9ff1d12940920100f3818a2e865628
KoenigOne <mm6dd.mh@gmail.com>
2025-10-05 21:32:11 +0100

    changed database path

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,10 @@
+const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./TMS-DB.db", (err) => {
+const dbPath = path.join(__dirname, "../TMS-DB.db");
+console.log("Connecting to DB at:", dbPath);
+
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Error opening database:", err.message);
   } else {
